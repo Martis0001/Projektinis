2bb0e02f6eb175e8ee0b849760f90e619b56cd8f
Moksleivis <Moksleivis@kitm.local>
2022-12-05 11:22:09 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,23 @@
+//1. susikurti JS kintamuosius - gauti reiksmes is input lauku
+//visi input
+const my_productCode = document.getElementById('productCode');
+const my_productName = document.getElementById('productName');
+const my_productQuantity = document.getElementById('productQuantity');
+const my_find_productCode = document.getElementById('find_productCode');
+//visi mygtukai
+const my_insertProduct = document.getElementById('insertProduct');
+const my_deleteProduct = document.getElementById('deleteProduct');
+const my_findProduct = document.getElementById('findProduct');
+//resultatus spausdinsiu i 
+const my_results = document.getElementById('results');
+//2. sukurti  funkcijas create, read, delete
+const createProduct = () => {
+  console.log(my_productCode.value, my_productName.value, my_productQuantity.value);
+
+ //patikrinti ar uzpildytas kodo laukas
+}
+//3. ant mygtuku uzdeti  funkciju iskvietimus
+
+
+//4. funkciju viduje darbas su local storage
+
